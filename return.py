9fe9b94c987e19225821216df68e820e71381179
Ahmed Rezik <ahmedrezq100@hotmail.com>
2021-04-13 03:37:58 +0200

    Added Log Returns

--- return.py
+++ return.py
@@ -1,9 +1,45 @@
 import numpy as np
 import pandas as pd
-import pandas_datareader.data as pdr
+from pandas_datareader import data as wb
 import yfinance as yf
 import matplotlib.pyplot as plt
+import re
 
-gme = yf.Ticker("GME")
-data = pd.DataFrame(gme.history(period="max"))
-print(data["Close"]["2021-03-16"])
+
+ticker = "AAPL"#eval(input("\x1b[1;32m Enter the underlying stock trading ticker \n \n some famous tickers: \n Google: GOOGL \t Apple: AAPL \n Tesla: TSLA \t Amazon: AMZN\n Netflix: nflx \t Ford Motors: F \n"))
+tkr = yf.Ticker(ticker)
+data = pd.DataFrame(tkr.history(period="max")) 
+data.to_csv(r'/Users/ahmed/Desktop/Bachelor/Markowitz-Simulator/aaple.csv', index = True)
+
+def netReturn(date1,date2):
+    # Main assumption here is that there are no dividends distributed 
+    # price @t / price @t-1  - 1
+    return (data["Close"][date2] -  data["Close"][date1]) / data["Close"][date1]
+
+
+def grossReturn():
+    print("HELLO")
+
+ClosingPrice = []
+for i in range(20):
+    ClosingPrice.append(data["Close"][str(2000+i)][0])
+
+logReturn = []
+for i in range(19):
+    logReturn.append(np.log(ClosingPrice[i+1]/ClosingPrice[i]))
+
+
+print(ClosingPrice)
+print(logReturn)
+
+    
+
+# print(data['Close']/data['Close'].shift(365))
+# data['Log Return'].plot(figsize=(18,5))
+# plt.show()
+
+# daily_avg_returns = data['Log Return'].mean()
+# print(daily_avg_returns)
+
+# annual_log_returns = data['Log Return'].mean() * 250
+# print(annual_log_returns)
